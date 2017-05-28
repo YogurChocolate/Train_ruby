7ab9862fc54809f53fc78b333c16614390080572
yuezhenxun <yuezhenxun@163.com>
2017-05-28 09:35:01 +0800

    qustion 1,The distance of the route A-B-C should be 9, add test, but getlength modth not make

--- spec/graph_spec.rb
+++ spec/graph_spec.rb
@@ -5,10 +5,14 @@ require '~/ruby/Train_ruby/spec/graph.rb'
 describe 'graph inited' do
 
   graph = Graph.new
-  require 'byebug'; byebug;
-  it 'get path len of abc is should 9' do
+
+  it 'graph should stored by excpte value' do
     excpte_store={"A"=>[["B", "5"], ["D", "5"], ["E", "7"]], "B"=>[["C", "4"]], "C"=>[["D", "8"], ["E", "2"]], "D"=>[["C", "8"], ["E", "6"]], "E"=>[["B", "3"]]}
     expect(graph.store).to eql excpte_store
+  end
+
+  it 'The distance of the route A-B-C should be 9' do
+    expect(graph.getlength('ABC')).to eql 9
   end
 
 end
