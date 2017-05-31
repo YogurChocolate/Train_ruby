bff04eefa299ba1259eabec91b9ca2b9a55258c9
yuezhenxun <yuezhenxun@163.com>
2017-05-31 17:57:43 +0800

    refactor rename

--- spec/graph_handler_spec.rb
+++ spec/graph_handler_spec.rb
@@ -20,19 +20,19 @@ describe 'graph inited by given input_value' do
   end
 
   it 'Get path num within max stop of start and end node same should retrun right path num' do
-    expect(graph_handler.get_path_num_within_max_node('C','C',3)).to eql 2
+    expect(graph_handler.get_trips_num_with_max_stops('C', 'C', 3)).to eql 2
   end
 
   it 'Get path num within max stop of start and end node different should retrun right path num' do
-    expect(graph_handler.get_path_num_within_exactly_node('A','C',4)).to eql 3
+    expect(graph_handler.get_trips_num_with_exactly_stops('A', 'C', 4)).to eql 3
   end
 
   it 'Get shortest route length of the start and end node different should retrun right' do
-    expect(graph_handler.get_shortest_route('A','C',0)).to eql 9
+    expect(graph_handler.get_shortest_route_length('A', 'C', 0)).to eql 9
   end
 
   it 'Get shortest route length of the start and end node same should retrun right' do
-    expect(graph_handler.get_shortest_route('B','B',0)).to eql 9
+    expect(graph_handler.get_shortest_route_length('B', 'B', 0)).to eql 9
   end
 
   it 'Get different routes num within max route length should be right' do
--- src/graph_handler.rb
+++ src/graph_handler.rb
@@ -18,17 +18,15 @@ class GraphHandler
     (0...nodes.length-1).each do |i|
       pre_node=nodes[i]
       next_node=nodes[i+1]
-      can_arivle_node_and_length=@store[pre_node]
-
-      is_available=false
-      can_arivle_node_and_length.each do |one_path|
-        if one_path[0]==next_node
-          is_available=true
-          route_distance+=one_path[1].to_i
+      is_reachable=false
+      @store[pre_node].each do |node_distance|
+        if node_distance[0]==next_node
+          is_reachable=true
+          route_distance+=node_distance[1]
           break
         end
       end
-      if !is_available
+      if !is_reachable
         return 'NO SUCH ROUTE'
       end
     end
@@ -36,40 +34,41 @@ class GraphHandler
   end
 
 
-  def get_path_num_within_max_node (start_node, end_node, max_stop)
-    path_num=0
-    if max_stop<0
+  def get_trips_num_with_max_stops (start_node, end_node, max_stops)
+    trips_num=0
+    if max_stops<0
       return 0
     end
+
     if start_node==end_node
-      path_num+=1
+      trips_num+=1
     end
 
-    @store[start_node].each do |one_path|
-      path_num+=get_path_num_within_max_node(one_path[0], end_node, max_stop-1).to_i
-      return path_num
+    @store[start_node].each do |node_distance|
+      trips_num+=get_trips_num_with_max_stops(node_distance[0], end_node, max_stops-1)
+      return trips_num
     end
   end
 
 
-  def get_path_num_within_exactly_node (start_node, end_node, exactly_stop)
-    path_num=0
-    if exactly_stop<0
+  def get_trips_num_with_exactly_stops (start_node, end_node, exactly_stops)
+    trips_num=0
+    if exactly_stops<0
       return 0
     end
 
-    if start_node==end_node && exactly_stop==0
-      path_num+=1
+    if start_node==end_node && exactly_stops==0
+      trips_num+=1
     end
 
-    @store[start_node].each do |one_path|
-      path_num+=get_path_num_within_exactly_node(one_path[0], end_node, exactly_stop-1).to_i
+    @store[start_node].each do |node_distance|
+      trips_num+=get_trips_num_with_exactly_stops(node_distance[0], end_node, exactly_stops-1)
     end
-    return path_num
+    return trips_num
   end
 
 
-  def get_shortest_route(start_node, end_node, already_pass_route_length=0)
+  def get_shortest_route_length(start_node, end_node, already_pass_route_length=0)
     if @already_find_one_shortest_route < already_pass_route_length
       return
     end
@@ -80,7 +79,7 @@ class GraphHandler
     end
 
     @store[start_node].each do |one_path|
-      get_shortest_route(one_path[0], end_node, already_pass_route_length+one_path[1])
+      get_shortest_route_length(one_path[0], end_node, already_pass_route_length+one_path[1])
     end
     return @already_find_one_shortest_route
   end
@@ -88,25 +87,25 @@ class GraphHandler
 
   def get_different_routes_num(start_node, end_node, max_route_length, already_pass_route_length=0)
     @already_find_routes_num=0
-    get_different_routes_num_function(start_node, end_node, max_route_length,already_pass_route_length)
+    get_different_routes_num_function(start_node, end_node, max_route_length, already_pass_route_length)
     return @already_find_routes_num
   end
 
 
-    private
+  private
 
-    def get_different_routes_num_function(start_node, end_node, max_route_length, already_pass_route_length)
-      if already_pass_route_length>=max_route_length
-        return
-      end
-
-      if start_node==end_node && already_pass_route_length!=0
-        @already_find_routes_num+=1
-      end
-
-      @store[start_node].each do |one_path|
-        get_different_routes_num_function(one_path[0], end_node, max_route_length, already_pass_route_length+one_path[1])
-      end
+  def get_different_routes_num_function(start_node, end_node, max_route_length, already_pass_route_length)
+    if already_pass_route_length>=max_route_length
+      return
     end
 
+    if start_node==end_node && already_pass_route_length!=0
+      @already_find_routes_num+=1
+    end
+
+    @store[start_node].each do |one_path|
+      get_different_routes_num_function(one_path[0], end_node, max_route_length, already_pass_route_length+one_path[1])
+    end
   end
+
+end
